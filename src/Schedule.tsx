0e46fea9a55d55fb7523769c4d4485e3ebe8c2c4
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-09-14 22:45:43 +0200

    fix: unify countdown for days
    This makes the countdown now show a unified
    difference in days.
    
    Closes #6

--- src/Schedule.tsx
+++ src/Schedule.tsx
@@ -6,9 +6,18 @@ import {
 	formatUserTime,
 	toUserTime,
 } from './time'
-import { formatDistance } from 'date-fns'
+import { formatDistance, differenceInCalendarDays } from 'date-fns'
 
-const diff = (startTime: Date) => formatDistance(startTime, new Date())
+const diff = (startTime: Date, conferenceDate: Date) => {
+	const now = new Date()
+	if (startTime.getTime() - Date.now() > 24 * 60 * 60 * 1000) {
+		// If difference is > 1 day
+		// show distance in days to conference start, so all entries have the same difference
+		const daysDistance = differenceInCalendarDays(conferenceDate, now)
+		return daysDistance > 1 ? `about ${daysDistance} days` : 'about 1 day'
+	}
+	return formatDistance(startTime, now)
+}
 
 const startsInMinutes = (startTime: Date) => {
 	return Math.floor((startTime.getTime() - Date.now()) / 1000 / 60)
@@ -17,18 +26,20 @@ const startsInMinutes = (startTime: Date) => {
 const Countdown = ({
 	startTime,
 	warnTime,
+	conferenceDate,
 }: {
 	startTime: Date
+	conferenceDate: Date
 	warnTime?: number
 }) => {
 	const [timeToStart, setTimeToStart] = React.useState({
-		text: diff(startTime),
+		text: diff(startTime, conferenceDate),
 		minutes: startsInMinutes(startTime),
 	})
 	React.useEffect(() => {
 		const interval = setInterval(() => {
 			setTimeToStart({
-				text: diff(startTime),
+				text: diff(startTime, conferenceDate),
 				minutes: startsInMinutes(startTime),
 			})
 		}, 1000 * 60)
@@ -38,7 +49,7 @@ const Countdown = ({
 	return (
 		<td
 			className={
-				timeToStart.minutes >= 0 && timeToStart.minutes <= (warnTime || 5)
+				timeToStart.minutes >= 0 && timeToStart.minutes <= (warnTime ?? 5)
 					? 'time hot'
 					: 'time'
 			}
@@ -94,6 +105,7 @@ export const Schedule = ({
 						</td>
 						<Countdown
 							key={conferenceDate}
+							conferenceDate={userTime(0)}
 							startTime={userTime((time as unknown) as number)}
 						/>
 						<td>{name}</td>
