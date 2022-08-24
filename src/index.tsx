0d199e8bd653e78848d0fbf8411138f499e79050
Markus Tacker <m@coderbyheart.com>
2022-08-24 12:42:08 +0200

    fix: update dependencies
    Closes #47
    Closes #46
    Closes #39
    Closes #38
    Closes #34
    Closes #33
    Closes #31
    Closes #27

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import { App } from 'app/App'
 import { log } from 'app/log'
 import { StrictMode } from 'react'
-import * as ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 const l = (...args: any) => log('App', ...args)
 l('Version:', import.meta.env.PUBLIC_VERSION)
 l('Source code:', import.meta.env.PUBLIC_HOMEPAGE)
 
-ReactDOM.render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-	document.getElementById('root'),
-)
+const container = document.getElementById('root')
+if (container !== null) {
+	const root = createRoot(container) // createRoot(container!) if you use TypeScript
+	root.render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	)
+} else {
+	console.error('Could not find root element!')
+}
